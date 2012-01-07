d9b9066562bd90605dce5de67e717d34835e0375
Shawn South <shawn.south@bellevuecollege.edu>
2012-01-06 19:06:02 -0800

    Started creating unit tests for EarlyStartFacet.

---
+++ DataFacets/EarlyStartFacet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using Ctc.Ods.Data;
+
+namespace Ctc.Ods
+{
+	public class EarlyStartFacet : ISectionFacet
+	{
+		/// <summary>
+		/// Provides the appropriate anonymous method for a LINQ .Where() call, depending on the option specified
+		/// </summary>
+		/// <param name="dbContext"></param>
+		public Expression<Func<T, bool>> GetFilter <T>(DbContext dbContext) where T : SectionEntity
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
