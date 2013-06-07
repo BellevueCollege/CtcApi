bde914ed5f6ce10ea23adf267f178778df31bf29
Shawn South <shawn.south@bellevuecollege.edu>
2013-06-07 12:29:31 -0700

    Added current date override capability to the ApplicationContext class.

--- CtcApi/ApplicationContext.cs
+++ CtcApi/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.DirectoryServices.AccountManagement;
 using System.Security.Principal;
 
@@ -6,8 +7,11 @@ namespace CtcApi
 {
 	public class ApplicationContext
 	{
-		private string _baseDirectory;
+	  private const string APPSETTINGS_KEY_CURRENT_DATE = "CurrentDate";
+
+	  private string _baseDirectory;
 	  private string _user;
+	  private DateTime? _currentDate;
 
 	  /// <summary>
     /// Gets or sets the value to use for <see cref="AppDomain.BaseDirectory"/>
@@ -34,29 +38,116 @@ namespace CtcApi
 		}
 
     /// <summary>
-    /// Gets the <see cref="WindowsIdentity.Name"/> or <see cref="UserPrincipal.UserPrincipalName"/> of the currently executing context.
+    /// Gets or sets the "current" date
     /// </summary>
-	  public string CurrentUser
+    /// <remarks>
+    /// <para>
+    ///   This property allows applications to override what the CtcApi believes the current
+    ///   date to be. Simply <c>set</c> the property to the date you want before making API calls.
+    /// </para>
+    /// <para>
+    ///   The <c>CurrentDate</c> can be modified in two ways:
+    /// </para>
+    /// <list type="table">
+    ///   <item>
+    ///     <term>Programmatically</term>
+    ///     <description>
+    ///       <example>
+    ///         Simply set the this property to the <see cref="DateTime"/> you wish it to be:
+    ///         <code>
+    ///           ApplicationContext context = new ApplicationContext();
+    ///           context.CurrentDate = DateTime.Parse("10/7/2013");
+    ///         </code>
+    ///       </example>
+    ///       <note type="note">
+    ///         Setting the <c>CurrentDate</c> programmatically overrides the .config file setting.
+    ///       </note>
+    ///     </description>
+    ///   </item>
+    ///   <item>
+    ///     <term>In the .config file</term>
+    ///     <description>
+    ///       <example>
+    ///         Specify the a date for the <i>CurrentDate</i> <c>appSettings</c>:
+    ///         <code>
+    ///           <appSettings>
+    ///             <add key="CurrentDate" value="10/7/2013"/>
+    ///           </appSettings>
+    ///         </code>
+    ///       </example>
+    ///       <note type="warning">
+    ///         If this value is not a valid <see cref="DateTime"/> it will be ignored.
+    ///       </note>
+    ///     </description>
+    ///   </item>
+    /// </list>
+    /// </remarks>
+	  public DateTime? CurrentDate
+	  {
+	    get
+	    {
+	      if (!_currentDate.HasValue)
+	      {
+          _currentDate = GetCurrentDate();
+	      }
+	      return _currentDate;
+	    }
+      set
+      {
+        _currentDate = value.HasValue ? value : GetCurrentDate();
+      }
+	  }
+
+	  #region Private members
+
+    /// <summary>
+    /// Retrieves "current" date from the .config file.
+    /// </summary>
+    /// <returns>
+    ///   The <see cref="DateTime"/> value of the <i>CurrentDate</i> setting if valid,
+    ///   otherwise <see cref="DateTime.Now"/>.
+    /// </returns>
+    private DateTime GetCurrentDate()
+    {
+      string setting = ConfigurationManager.AppSettings[APPSETTINGS_KEY_CURRENT_DATE];
+
+      DateTime currentDate;
+      if (!string.IsNullOrWhiteSpace(setting))
+      {
+        DateTime dt;
+        currentDate = DateTime.TryParse(setting, out dt) ? dt : DateTime.Now;
+      }
+      else
+      {
+        currentDate = DateTime.Now;
+      }
+      return currentDate;
+    }
+
+    #region UNDER DEVELOPMENT
+	  /// <summary>
+	  /// Gets the <see cref="WindowsIdentity.Name"/> or <see cref="UserPrincipal.UserPrincipalName"/> of the currently executing context.
+	  /// </summary>
+	  private string CurrentUser
 	  {
 	    get
 	    {
 	      _user = GetCurrentUser(_user);
-        return _user;
+	      return _user;
 	    }
 	  }
 
-    /// <summary>
-    /// Gets the <see cref="WindowsIdentity.Name"/> or <see cref="UserPrincipal.UserPrincipalName"/> of the currently executing context.
-    /// </summary>
-    /// <returns>
-    /// The <i>username</i> or <i>login</i> of the currently running account.
-    /// </returns>
-    public static string GetCurrentUser()
-    {
-      return GetCurrentUser(null);
-    }
+	  /// <summary>
+	  /// Gets the <see cref="WindowsIdentity.Name"/> or <see cref="UserPrincipal.UserPrincipalName"/> of the currently executing context.
+	  /// </summary>
+	  /// <returns>
+	  /// The <i>username</i> or <i>login</i> of the currently running account.
+	  /// </returns>
+	  private static string GetCurrentUser()
+	  {
+	    return GetCurrentUser(null);
+	  }
 
-	  #region Private members
 	  /// <summary>
 	  /// Gets the <see cref="WindowsIdentity.Name"/> or <see cref="UserPrincipal.UserPrincipalName"/> of the currently executing context.
 	  /// </summary>
@@ -97,6 +188,8 @@ namespace CtcApi
 
 	    return user;
 	  }
+
+	  #endregion
 
 	  #endregion
 	}
---
+++ Test.CtcApi/ApplicationContextTest.cs
@@ -0,0 +1,156 @@
+﻿using System.Configuration;
+using CtcApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.CtcApi
+{
+  /// <summary>
+  ///This is a test class for ApplicationContextTest and is intended
+  ///to contain all ApplicationContextTest Unit Tests
+  ///</summary>
+  [TestClass()]
+  public class ApplicationContextTest
+  {
+
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+
+    #region CurrentDate tests
+    [TestMethod()]
+    public void CurrentDateTest_DefaultNow()
+    {
+      ApplicationContext target = new ApplicationContext();
+      DateTime? expected = DateTime.Now;
+      DateTime? actual = target.CurrentDate;
+
+      Assert.IsTrue(actual.HasValue, "CurrentDate returned NULL.");
+      Assert.AreEqual(expected.Value.ToShortDateString(), actual.Value.ToShortDateString());
+    }
+
+    [TestMethod()]
+    public void CurrentDate_SetProgrammatically()
+    {
+      DateTime? expected = DateTime.Parse("10/7/2013");
+      ApplicationContext target = new ApplicationContext
+                                    {
+                                      CurrentDate = expected
+                                    };
+      DateTime? actual = target.CurrentDate;
+
+      Assert.IsTrue(actual.HasValue, "CurrentDate returned NULL.");
+
+      string shortDateString = actual.Value.ToShortDateString();
+
+      Assert.AreNotEqual(DateTime.Now.ToShortDateString(), shortDateString);
+      Assert.AreEqual(expected.Value.ToShortDateString(), shortDateString);
+    }
+
+    [TestMethod()]
+    public void CurrentDate_FromConfig()
+    {
+      ConfigurationManager.AppSettings["CurrentDate"] = "7/20/2010";
+
+      DateTime? expected = DateTime.Parse(ConfigurationManager.AppSettings["CurrentDate"]);
+      ApplicationContext target = new ApplicationContext();
+      DateTime? actual = target.CurrentDate;
+
+      Assert.IsTrue(actual.HasValue, "CurrentDate returned NULL.");
+
+      string shortDateString = actual.Value.ToShortDateString();
+
+      Assert.AreNotEqual(DateTime.Now.ToShortDateString(), shortDateString);
+      Assert.AreEqual(expected.Value.ToShortDateString(), shortDateString);
+    }
+
+    [TestMethod()]
+    public void CurrentDate_InvalidDateFromConfig()
+    {
+      ConfigurationManager.AppSettings["CurrentDate"] = "foo";
+
+      ApplicationContext target = new ApplicationContext();
+      DateTime? actual = target.CurrentDate;
+
+      Assert.IsTrue(actual.HasValue, "CurrentDate returned NULL.");
+
+      Assert.AreEqual(DateTime.Now.ToShortDateString(), actual.Value.ToShortDateString());
+    }
+
+    [TestMethod()]
+    public void CurrentDate_ProgrammaticallyResetToNow()
+    {
+      ApplicationContext target = new ApplicationContext();
+      target.CurrentDate = null;
+      DateTime? actual = target.CurrentDate;
+
+      Assert.IsTrue(actual.HasValue, "CurrentDate returned NULL.");
+
+      Assert.AreEqual(DateTime.Now.ToShortDateString(), actual.Value.ToShortDateString());
+    }
+
+    #endregion
+
+    /// <summary>
+    ///A test for BaseDirectory
+    ///</summary>
+    [TestMethod()]
+    [Ignore]
+    public void BaseDirectoryTest()
+    {
+      ApplicationContext target = new ApplicationContext(); // TODO: Initialize to an appropriate value
+      string expected = string.Empty; // TODO: Initialize to an appropriate value
+      string actual;
+      target.BaseDirectory = expected;
+      actual = target.BaseDirectory;
+      Assert.AreEqual(expected, actual);
+      Assert.Inconclusive("Verify the correctness of this test method.");
+    }
+  }
+}
