6a47d0fb5d159509e0c8e1f7e9b0f89336ae61fd
Nicole Swan <nicole.swan@bellevuecollege.edu>
2017-09-08 15:38:46 -0700

    Update GetGroups() to specific PrincipalContext
    - Also had to update logger

--- CtcApi/Properties/AssemblyInfo.cs
+++ CtcApi/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Washington State Board Community and Technical Colleges - Information Technology (SBCTC-IT)")]
 [assembly: AssemblyProduct("CtcApi")]
-[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.15.0")]
-[assembly: AssemblyFileVersion("0.9.15.0")]
+[assembly: AssemblyVersion("0.9.16.1")]
+[assembly: AssemblyFileVersion("0.9.16.1")]
